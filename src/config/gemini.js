3c8299c5b816569b6fc0fe204641a1ca4ebd75b0
Karan Hemraj Patil <karanhemrajpatil1907@gmail.com>
2024-06-05 08:50:53 +0530

    security updates done

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -13,7 +13,7 @@ import {
   HarmBlockThreshold,
 } from "@google/generative-ai";
 
-const apiKey = 'AIzaSyCdgjmLcod0_v04CzYIfdvzitxFnJAkXjI';
+const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
